86033a41bb54873cd68af17ef77d6da21a295eae
Charley Peng <cpeng92@gmail.com>
2011-01-11 18:16:59 +1100

    added testing for dictCompletions

---
+++ dictCompletions.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This file tests the dictComlpetionsN
+gcc -o cov_word words.c dictionary.c dictionary.h -g -Wall -fprofile-arcs -ftest-coverage
+./cov_word d < tc6
+./cov_word delta < tc6
+gcov dictionary.c
+vi +/138 dictionary.c.gcov 
